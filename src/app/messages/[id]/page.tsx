e8228104aac7d4602b3cda121b8b49e170b1cd45
GairikSharma <gairiksharma202@gmail.com>
2024-05-13 02:08:31 +0530

    comment section update

--- src/app/messages/[id]/page.tsx
+++ src/app/messages/[id]/page.tsx
@@ -181,7 +181,7 @@ export default function Home(obj: parameter) {
           </div>
 
 
-          <div className="flex flex-col w-full md:w-[50%] lg:w-[50%] h-screen">
+          <div className="flex flex-col-reverse md:flex-col lg:flex-col w-full md:w-[50%] lg:w-[50%] h-screen">
             <div className="h-[78%] overflow-auto">
               <div className="p-4 font-[800] mt-8 mb-4">{`Comments (${comment.length})`}</div>
               {comment.map((item: com) => {
@@ -197,8 +197,10 @@ export default function Home(obj: parameter) {
                 );
               })}
             </div>
+            
             <div className="flex flex-row m-4 rounded-md ">
-              <div className="w-[90%] h-[50px] bg-red-400 p-2 mr-2   border-[2px] font-[600] border-black justify-center items-center rounded-md">
+              
+              <div className="w-[90%] h-[50px] bg-red-400 p-2 mr-2 border-[2px] font-[600] border-black justify-center items-center rounded-md">
                 <input
                   type="text"
                   className="w-full h-full justify-center items-center m-0 p-0 bg-red-400 focus:outline-none "
